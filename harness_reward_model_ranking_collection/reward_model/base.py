04744b05b9c17bc3f1183842e3a8b9ebe030698b
tonyguo <tony92151@gmail.com>
2024-05-13 20:16:55 +0800

    test

--- harness_reward_model_ranking_collection/reward_model/base.py
+++ harness_reward_model_ranking_collection/reward_model/base.py
@@ -4,10 +4,10 @@ class BaseRewardModel:
 
     def get_reward_candidates(
         self, instruction: str, candidates: list[str], top_k: int = 3
-    ) -> list[str]:
+    ) -> tuple[list[list[str]], list[list[float]]]:
         raise NotImplementedError
     
     def get_batch_reward_candidates(
         self, instruction: list[str], candidates: list[list[str]], top_k: int = 3
-    ) -> list[list[str]]:
+    ) -> tuple[list[list[str]], list[list[float]]]:
         pass
