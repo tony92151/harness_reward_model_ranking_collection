e96c561c6540af6a71d3b362f6be0fac04c97a4a
tonyguo <tony92151@gmail.com>
2024-05-13 20:16:05 +0800

    add batch rank llm blender

--- harness_reward_model_ranking_collection/reward_model/base.py
+++ harness_reward_model_ranking_collection/reward_model/base.py
@@ -6,3 +6,8 @@ class BaseRewardModel:
         self, instruction: str, candidates: list[str], top_k: int = 3
     ) -> list[str]:
         raise NotImplementedError
+    
+    def get_batch_reward_candidates(
+        self, instruction: list[str], candidates: list[list[str]], top_k: int = 3
+    ) -> list[list[str]]:
+        pass
--- harness_reward_model_ranking_collection/reward_model/llm_blender/llm_blender.py
+++ harness_reward_model_ranking_collection/reward_model/llm_blender/llm_blender.py
@@ -37,6 +37,10 @@ class LlmBlenderPipe(BaseRewardModel):
 
         self.llm_blender = init_llm_blender(use_fuser=self.use_fuser)
 
+
+        self.batch_size = kwargs.get("batch_size", 4)
+        self.stop_sequences = kwargs.get("stop_sequences", [])
+
     def get_reward_candidates(
         self, instruction: str, candidates: list[str], top_k: int = 3
     ) -> tuple[list[str], list[float]]:
@@ -70,3 +74,37 @@ class LlmBlenderPipe(BaseRewardModel):
         time_cost = time.time() - t
 
         return topk_candidates_list, [time_cost, time_cost]
+    
+    def get_batch_reward_candidates(self, instruction: list[str], candidates: list[list[str]], top_k: int = 3) -> tuple[list[list[str]], list[list[float]]]:
+        t = time.time()
+        ranks = self.llm_blender.rank(
+            instruction,
+            candidates,
+            # instructions=None,
+            return_scores=False,
+            batch_size=self.batch_size,
+            disable_tqdm=True,
+        )
+
+        topk_candidates = get_topk_candidates_from_ranks(
+            ranks, candidates, top_k=top_k
+        )
+        batch_topk_candidates_list = []
+        for topk_candidate in topk_candidates:
+            batch_topk_candidates_list.append(topk_candidate.tolist())
+
+        if self.use_fuser:
+            generate_kwargs = {"max_new_tokens": self.max_new_tokens}
+            fuse_generations = self.llm_blender.fuse(
+                instruction,
+                topk_candidates,
+                instructions=None,
+                batch_size=self.batch_size,
+                stop_sequences=self.stop_sequences,
+                **generate_kwargs
+            )
+            batch_topk_candidates_list = [[fuse_generation]for fuse_generation in fuse_generations]
+
+        time_cost = time.time() - t
+
+        return batch_topk_candidates_list, [[time_cost, time_cost] for _ in batch_topk_candidates_list]
